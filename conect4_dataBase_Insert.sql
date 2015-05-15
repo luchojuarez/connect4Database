2f006c892a931c03c7a6b4a557867c3b19033284
jicibils <jicibils@hotmail.com>
2015-05-15 09:54:53 -0300

    todo listo para empezar con los leopardos

---
+++ conect4_dataBase_Insert.sql
@@ -0,0 +1,5 @@
+--  conect4_dataBase_Insert.sql
+
+
+-- en el leopardo cuando insertemos los ex user agregar "user" o "current_user" para que me devuelva
+-- quien elimono al usuario... la funcion se invoca sin parentesis...
